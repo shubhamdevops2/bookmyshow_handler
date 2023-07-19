ec2c51d9e10308caea44f729d2a5754879a681fb
shubhamdevops2 <shubhamd.devops.2@gmail.com>
2023-07-19 12:23:09 +0530

    sds

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 
 // MongoDB connection URI
-const mongoURI = 'mongodb://admin:VRuAd2Nvmp4ELHh5@192.168.0.108:27017';
+const mongoURI = 'mongodb://${process.env.DB_USER}:${process.env.DB_PW}@handler-svc:27017';
 
 // MongoDB connection options
 const mongoOptions = {
